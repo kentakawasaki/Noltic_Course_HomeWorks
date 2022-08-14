e7e94212e23771c8fa2f6348e3055c07ed3037c4
Viktor Manko <37028565+kentakawasaki@users.noreply.github.com>
2022-08-15 00:11:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function lengthConvertertoSm(valNum) {
+  document.getElementById("inputSaMetersDm").value = valNum / 0.3937;
+}
+function lengthConvertertoDume(valNum) {
+  document.getElementById("inputDume").value = valNum * 0.3937;
+}
+
+function lengthConvertertoFuteToSm(valNum) {
+  document.getElementById("inputFute").value = valNum * 0.032808;
+}
+function lengthConvertertoSmtOFute(valNum) {
+  document.getElementById("inputSmFt").value = valNum / 0.032808;
+}
+
+function lengthConvertertoMeterstoYard(valNum) {
+  document.getElementById("inputYard").value = valNum * 0.9144;
+}
+function lengthConvertertoYardtoMeters(valNum) {
+  document.getElementById("inputM").value = valNum / 0.9144;
+}
+
+function lengthConvertertoKilomiterstoMiles(valNum) {
+  document.getElementById("inputMiles").value = valNum * 0.621371192;
+}
+function lengthConvertertoMilestoKilmiters(valNum) {
+  document.getElementById("inputKilomiters").value = valNum / 0.621371192;
+}
